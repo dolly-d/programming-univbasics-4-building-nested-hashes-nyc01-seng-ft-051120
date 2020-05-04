b1881f11e6026d9faf2cde56e14856eebecc61cb
Dolly Desir <dolly.desir@gmail.com>
2020-05-04 02:17:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,9 +32,11 @@ hash = {
       :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200},
       :names => {
         :reading_railroad => {},
-        :pennsylvania_railraod => {}
-     }
-  }
+        :pennsylvania_railraod => {},
+        :b_and_o_railroad => {},
+        :shortline_railroad => {}
+      }
+   }
   
 end
 
